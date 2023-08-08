8b3f53b24714f18a6635c52d3167ed9f9093c0bc
AlthafAhd007 <althafahamed07@gmail.com>
2023-08-08 16:36:37 +0530

    changed the ports

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World! edited inside remote container");
 });
 
-app.listen(5000, () => {
-  console.log(`Server started at port 5000`);
+app.listen(7000, () => {
+  console.log(`Server started at port 7000`);
 });
